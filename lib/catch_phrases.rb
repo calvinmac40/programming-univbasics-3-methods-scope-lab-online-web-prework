bbe7c1eca3f52fcd773d6ecf02bbbace1d4c59b9
Calvin McDowell <calvinmac40@hotmail.com>
2019-07-07 13:51:27 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -13,8 +13,8 @@ def link
   puts phrase
 end
 
-def any_phrase(phrase = "Do A Barrel Roll!")
+def any_phrase(phrase)
   puts "I like to #{phrase}"
 end
 
-any_phrase
+any_phrase("Do A Barrel Roll!")
